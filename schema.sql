d7098b19462bbe0d1287196aa1d0701feecd4d00
Caren-Koroeny <carensiya5@gmail.com>
2023-05-01 19:55:57 +0300

    create table called animal

---
+++ schema.sql
@@ -0,0 +1,9 @@
+/* Database schema to keep the structure of entire database. */
+CREATE TABLE animals (
+   id SERIAL PRIMARY KEY,
+   name VARCHAR(50) NOT NULL,
+   date_of_birth DATE NOT NULL,
+   escape_attempts INTEGER NOT NULL,
+   neutered BOOLEAN NOT NULL,
+   weight_kg DECIMAL(5,2) NOT NULL
+);
