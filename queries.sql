d56def81996b2656f496876f9a8cf94839313b1f
Caren-Koroeny <carensiya5@gmail.com>
2023-05-02 09:48:12 +0300

    Who escapes the most, neuteret or not neautred animals

--- queries.sql
+++ queries.sql
@@ -67,3 +67,7 @@ SELECT COUNT(*) FROM animals;
 SELECT COUNT(*) FROM animals WHERE escape_attempts > 0;
 -- What is the average weight of animals?
 SELECT AVG(weight_kg) FROM animals;
+-- Who escapes the most, neutered or not neutered animals?
+SELECT neutered, AVG(escape_attempts) AS escapes FROM animals GROUP BY neutered;
+
+
