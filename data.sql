baa40e30a0f8d482d5ffa95989c58531697ac6ef
Caren-Koroeny <carensiya5@gmail.com>
2023-05-03 11:56:19 +0300

    Update Dean to Angemon and Boarmon

--- data.sql
+++ data.sql
@@ -59,3 +59,7 @@ WHERE name IN ('Devimon','Plantmon');
 UPDATE animals
 SET owner_id = (SELECT id FROM owners WHERE full_name = 'Melody Pond')
 WHERE name IN ('Charmander','Squirtle','Blossom');
+
+UPDATE animals
+SET owner_id = (SELECT id FROM owners WHERE full_name = 'Dean Winchester')
+WHERE name IN ('Angemon','Boarmon');
