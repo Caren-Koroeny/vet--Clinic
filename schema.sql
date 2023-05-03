ccaa04b7ab4499f622d42ad5f72f85d07ae85958
Caren-Koroeny <carensiya5@gmail.com>
2023-05-03 06:21:05 +0300

    Add a column species of type string to the table

--- schema.sql
+++ schema.sql
@@ -14,6 +14,12 @@ CREATE TABLE IF NOT EXISTS animals (
 
 -- Update made the id column not null
 ALTER TABLE animals ALTER COLUMN id SET NOT NULL;
+--Update made the id column the Primary key for the table
+ALTER TABLE animals ADD PRIMARY KEY(id);
+-- Update made the id of animals table be auto incremented
+ALTER TABLE animals ALTER COLUMN id ADD GENERATED ALWAYS AS IDENTITY;
+--Update Add a column species of type string to your animals table.
+ALTER TABLE animals ADD COLUMN species VARCHAR(80);
 
 ALTER TABLE animals
   ADD species VARCHAR;
