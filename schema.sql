60a88360c9af68aaebf4292a61bc78736b5a4fd5
Caren-Koroeny <carensiya5@gmail.com>
2023-05-03 06:16:11 +0300

    made all id column not null

--- schema.sql
+++ schema.sql
@@ -1,11 +1,19 @@
 /* Database schema to keep the structure of entire database. */
-CREATE TABLE animals (
-   id SERIAL PRIMARY KEY,
-   name VARCHAR(50) NOT NULL,
-   date_of_birth DATE NOT NULL,
-   escape_attempts INTEGER NOT NULL,
-   neutered BOOLEAN NOT NULL,
-   weight_kg DECIMAL(5,2) NOT NULL
+CREATE DATABASE vet_clinic;
+
+DROP TABLE IF EXISTS animals;
+
+CREATE TABLE IF NOT EXISTS animals (
+    id INT,
+    name VARCHAR(80),
+    date_of_birth DATE,
+    escape_attempts INT,
+    neutered BOOLEAN,
+    weight_kg NUMERIC
 );
+
+-- Update made the id column not null
+ALTER TABLE animals ALTER COLUMN id SET NOT NULL;
+
 ALTER TABLE animals
   ADD species VARCHAR;
