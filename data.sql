c53babf5bb30cf2d197039c156587138166a6571
Caren-Koroeny <carensiya5@gmail.com>
2023-05-04 13:15:33 +0300

    Insert visits for Stephen and Agumon

--- data.sql
+++ data.sql
@@ -86,3 +86,9 @@ INSERT INTO visits(animal_id, vet_id, date_of_visit) VALUES (
   (SELECT id FROM vets WHERE name = 'William Tatcher'),
   '2020-05-24' 
 );
+
+INSERT INTO visits(animal_id, vet_id, date_of_visit) VALUES (
+  (SELECT id FROM animals WHERE name = 'Agumon'),
+  (SELECT id FROM vets WHERE name = 'Stephanie Mendez'),
+  '2020-07-22' 
+);
