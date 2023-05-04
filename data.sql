a941503ce6e33a4287fb329c69582db691335290
Caren-Koroeny <carensiya5@gmail.com>
2023-05-04 13:01:03 +0300

    Insert data for vets

--- data.sql
+++ data.sql
@@ -66,3 +66,10 @@ WHERE name IN ('Angemon','Boarmon');
 
 -- Day 4
 
+INSERT INTO vets(name,age,date_of_graduation) VALUES
+('William Tatcher', 45, '2000-04-23'),
+('Maisy Smith', 26, '2019-01-17'),
+('Stephanie Mendez', 64, '1981-05-04'),
+('Jack Harkness', 38, '2008-06-08');
+
+
